ff3dbd3b0b45d37e3f730cf130ac64a1daee64f1
Astle <67687557+astledsa@users.noreply.github.com>
2025-02-28 15:06:09 +0530

    Update sam.py

--- sam.py
+++ sam.py
@@ -1,13 +1,8 @@
-import base64
-import requests
-import numpy as np
-from segment_anything import sam_model_registry, SamPredictor
-
 def Init_predictor () -> SamPredictor:
     """
     Initialize the model
     """
-    checkpoint: str = "models/sam_vit_h_4b8939.pth"
+    checkpoint: str = "/kaggle/input/sam/pytorch/default/1/sam_vit_h_4b8939.pth"
     model_type: str = "vit_h"
     sam = sam_model_registry[model_type](checkpoint=checkpoint)
     return SamPredictor(sam)
@@ -22,38 +17,32 @@ def normalized_to_original(width: float, height: float, x: float, y: float) -> n
     return np.array([[original_x, original_y]])
 
 def Masked (
-        predictor: SamPredictor,
-        image: np.ndarray,
-        point: np.array,
-        normalize: bool = False,
+        predictor: SamPredictor, 
+        image: np.ndarray, 
+        point: np.array, 
         mask_it: bool = False
     ) -> np.ndarray:
-   """
-   Returns a mask of the image. If mask_it is set to true, this functions applies the
-   mask to the image, ans returns the new image.
-   """
-   input_point = normalized_to_original(image.shape[0], image.shape[1], point[0][0], point[0][1]) if normalize else point
-   input_label = np.array([1])
-
-   predictor.set_image(image)
-   masks, _, _ = predictor.predict(
-      point_coords=input_point,
-      point_labels=input_label,
-      multimask_output=False,
+    
+    print("Image shape: ", image.shape)
+    input_label = np.array([1])
+    predictor.set_image(image)
+    masks, _, _ = predictor.predict(
+        point_coords=point,
+        point_labels=input_label,
+        multimask_output=False,
     )
-
-   if mask_it:
-      masked_image = np.copy(image)
-      masked_image[masks.squeeze() == 0] = 0
-      return masked_image
-   else:
-       return masks
-
+    if mask_it: 
+        masked_image = np.copy(image)
+        masked_image[masks.squeeze() == 0] = 0
+        return masked_image
+    else:
+        return masks
 
 def image_array_to_base64 (arr: np.array) -> str:
     """
     Converts a numpy array to base64 string
     """
+    print("Trying here")
     pil_image = Image.fromarray(arr)
     buffer = BytesIO()
     pil_image.save(buffer, format="PNG")
